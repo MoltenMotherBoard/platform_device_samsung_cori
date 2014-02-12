6892d51ce35cdc115b0317c6ee00e1ca4d602348
Davide Pucci <davidepucci21@gmail.com>
2014-02-12 16:09:41 +0100

    Avoid problems of inclusions for right boot.img
    Adapted shbootimg.mk to tighten all the code in a single function ~ $(INSTALLED_BOOTIMAGE_TARGET).
    In fact, while making an otapackage, it used to include the old boot.img (the result of first 10 lines of old shbootimg.mk) and while flashing the zip, we had a bootloop.
    New shbootimg.mk has now all the needed code under $(INSTALLED_BOOTIMAGE_TARGET) and then the right - and working/booting - boot.img is included properly in the zip.

--- shbootimg.mk
+++ shbootimg.mk
@@ -1,11 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
-$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES)
-	$(call pretty,"Target boot image: $@")
-	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) --output $@
-	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
-
-$(TARGET_RECOVERY_ROOT_TIMESTAMP): $(INTERNAL_RECOVERY_FILES) \
+$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) \
+		$(INTERNAL_BOOTIMAGE_FILES) \
+		$(INTERNAL_RECOVERY_FILES) \
 		$(INSTALLED_RAMDISK_TARGET) \
 		$(INSTALLED_BOOTIMAGE_TARGET) \
 		$(recovery_binary) \
@@ -14,15 +11,16 @@ $(TARGET_RECOVERY_ROOT_TIMESTAMP): $(INTERNAL_RECOVERY_FILES) \
 		$(recovery_build_prop) $(recovery_resource_deps) \
 		$(recovery_fstab) \
 		$(RECOVERY_INSTALL_OTA_KEYS) \
-		$(INSTALLED_BOOTIMAGE_TARGET)
-	@echo ----- Making recovery filesystem ------
+		$(INSTALLED_BOOTIMAGE_TARGET) \
+		$(MKBOOTFS) \
+		$(MKBOOTIMG) \
+		$(recovery_kernel)
+	@echo ----- Executing shbootimg.mk ------
 	mkdir -p $(TARGET_RECOVERY_OUT)
 	mkdir -p $(TARGET_RECOVERY_ROOT_OUT)
 	mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/etc
 	mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/tmp
-	echo Copying baseline ramdisk...
 	cp -R $(TARGET_ROOT_OUT) $(TARGET_RECOVERY_OUT)
-	echo Modifying ramdisk contents...
 	cp -f $(recovery_initrc) $(TARGET_RECOVERY_ROOT_OUT)/
 	cp -f $(recovery_binary) $(TARGET_RECOVERY_ROOT_OUT)/sbin/
 	mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/system/bin
@@ -35,24 +33,10 @@ $(TARGET_RECOVERY_ROOT_TIMESTAMP): $(INTERNAL_RECOVERY_FILES) \
 	  cp -f $(item) $(TARGET_RECOVERY_ROOT_OUT)/etc/recovery.fstab)
 	cp $(RECOVERY_INSTALL_OTA_KEYS) $(TARGET_RECOVERY_ROOT_OUT)/res/keys
 	cp $(BOARD_CUSTOM_PROP_TARGET) $(TARGET_RECOVERY_ROOT_OUT)/default.prop
-	@echo ----- Made recovery filesystem -------- $(TARGET_RECOVERY_ROOT_OUT)
 	@touch $(TARGET_RECOVERY_ROOT_TIMESTAMP)
-
-$(recovery_uncompressed_ramdisk): $(MINIGZIP) \
-    $(TARGET_RECOVERY_ROOT_TIMESTAMP)
-	@echo ----- Making uncompressed recovery ramdisk ------
-	$(MKBOOTFS) $(TARGET_RECOVERY_ROOT_OUT) > $@
-
-$(recovery_ramdisk): $(MKBOOTFS) \
-    $(recovery_uncompressed_ramdisk)
-	@echo ----- Making recovery ramdisk ------
-	$(MINIGZIP) < $(recovery_uncompressed_ramdisk) > $@
-
-$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
-		$(recovery_ramdisk) \
-		$(recovery_kernel)
-	@echo ----- Making recovery image ------
-	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) --output $@
-	@echo ----- Made recovery image -------- $@
-	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
-	$(hide) cp -f $(INSTALLED_RECOVERYIMAGE_TARGET) $(INSTALLED_BOOTIMAGE_TARGET) 
+	$(MKBOOTFS) $(TARGET_RECOVERY_ROOT_OUT) > $(recovery_uncompressed_ramdisk)
+	$(MINIGZIP) < $(recovery_uncompressed_ramdisk) > $(recovery_ramdisk)
+	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) --output $(INSTALLED_RECOVERYIMAGE_TARGET)
+	$(hide) $(call assert-max-image-size,$(INSTALLED_RECOVERYIMAGE_TARGET),$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
+	$(hide) cp -f $(INSTALLED_RECOVERYIMAGE_TARGET) $@ 
+	@echo ----- Correctly compiled shbootimg.mk ------
